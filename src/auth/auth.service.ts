e733e4260583c230f6c967085879d99fd90d5151
Elanrif <saidbacoelanrif@gmail.com>
2025-01-10 07:51:11 +0100

    feat: password hash

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   async register(registerDto) {
     const { password } = registerDto;
-    return password;
+    const hashedPassword = await hash(password, 10);
+    return hashedPassword;
   }
 }
